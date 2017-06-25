1d07a0e4d3b6186d5b9175cd6a70898fc953287e
dingyi <dingyi@meican.com>
2017-06-25 18:22:08 +0800

    Fix puncutations and require error, add Gemfile and update wording

--- mission.rb
+++ mission.rb
@@ -2,8 +2,8 @@
 require 'telegram/bot'
 require 'json'
 require 'uri'
-require 'json_save.rb'
-token = 'token'
+require './json_save.rb'
+token = 'REPLACE THIS WITH THE BOT TOKEN'
 incompleteLabels = ['5666779e19ad3a5dc26426a5','57287baf9148b133b928f6da','56d4fd5d152c3f92fd3a75c7','574c64565b9b3323fb39a5bd']
 
 begin
@@ -20,7 +20,7 @@ begin
 						next if s_mission_title == '[' || s_mission_title == ']'
 						trello = open('ingress-medal-arts.json').read
 						if trello.nil?
-							bot.api.send_message(chat_id: message.chat.id, text: "出错辣,豆腐丝快粗来化身水管道工人") 
+							bot.api.send_message(chat_id: message.chat.id, text: "出错辣，快召唤 @Miaonster") 
 							next
 						end
 						json = JSON.parser.new(trello)
@@ -40,7 +40,7 @@ begin
 							end
 						}
 
-						bot.api.send_message(chat_id: message.chat.id, text: "任务已找到,请稍候喔~") if !mission_title.empty?
+						bot.api.send_message(chat_id: message.chat.id, text: "任务已找到，请稍候喔~") if !mission_title.empty?
 						if !mission_title.empty? && mission_title.length > 1
 							mission_title.each_with_index do |id,key|
 								uniq_title.push([cards_hash[id]['name'],cards_hash[id]['idLabels']])
@@ -51,7 +51,7 @@ begin
 								#result << "#{id}\n"
 								result << ((incompleteLabels+value[1]).uniq! == nil ? "\n" : " (incomplete)\n")
 							end
-							bot.api.send_message(chat_id: message.chat.id, text: "喏,一共有这么多任务,你要看哪一个呢: \n#{result}")
+							bot.api.send_message(chat_id: message.chat.id, text: "喏，一共有这么多任务，你要看哪一个呢：\n#{result}")
 						elsif mission_title.length == 1
 							result << "任务名:#{cards_hash[mission_title[0]]['name']}"
 							result << ((incompleteLabels+cards_hash[mission_title[0]]['idLabels']).uniq! == nil ? "\n" : " (incomplete)\n")
@@ -73,26 +73,26 @@ begin
 							end
 							bot.api.send_message(chat_id: message.chat.id, text: "#{result}" ,parse_mode:"Markdown", disable_web_page_preview: "true")
 						else
-							bot.api.send_message(chat_id: message.chat.id, text: "很抱歉没有查到你想找的任务信息~要不要换个姿势呢?")
+							bot.api.send_message(chat_id: message.chat.id, text: "很抱歉没有查到你想找的任务信息，要不要换个姿势呢？")
 						end
-					when '/q@today520_bot'
-						bot.api.send_message(chat_id: message.chat.id, text: "查询任务格式为: /q 任务名\n（建议大家如果仅仅是搜索trello可以小窗bot，以免对群组成员造成垃圾信息骚扰）")
+					when '/q@ingress_mission_arts_bot'
+						bot.api.send_message(chat_id: message.chat.id, text: "查询任务格式为: /q 任务名\n（建议大家如果仅仅是搜索 trello 可以小窗 bot，以免对群组成员造成垃圾信息骚扰）")
 					end
-			rescue 
-				bot.api.send_message(chat_id: message.chat.id, text: "出错辣,豆腐丝已启动强大自我修复机制，一分钟后如果还自动修不好，先检查trello格式是否符合MarkDown规范，如果还不行，再小窗豆腐丝 @tolves 哟") 
+			rescue => e
+				bot.api.send_message(chat_id: message.chat.id, text: "出错辣，启动强大自我修复机制，一分钟后如果还自动修不好，先检查 trello 格式是否符合 MarkDown 规范，如果还不行，再召唤 @Miaonster 哟") 
 				uri = URI('https://trello.com/b/LvwOjrYP/ingress-medal-arts.json')
 				save(uri)
+				puts "uncaught #{e} exception while handling connection: #{e.message}"
+				puts "Stack trace: #{e.backtrace.map {|l| "  #{l}\n"}.join}"
 				sleep(70)
 				retry
 			end
 		end
 	end
-rescue 
-  puts '又出错一次啦,人家先睡70s喔'		
-  sleep(70)
-  retry
+rescue => e
+	puts "uncaught #{e} exception while handling connection: #{e.message}"
+	puts "Stack trace: #{e.backtrace.map {|l| "  #{l}\n"}.join}"
+	puts '又出错一次啦，人家先睡 70s 喔'
+ 	sleep(70)
+ 	retry
 end
-
-
-
-
---
+++ save-trello-json.rb
@@ -0,0 +1,4 @@
+require './trello-json.rb'
+
+uri = URI('https://trello.com/b/LvwOjrYP/ingress-medal-arts.json')
+save(uri)
